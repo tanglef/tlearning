179bcf3286ff793b49ca0e6df4391b72318f4782
Tanguy Lefort <tanguy_l@outlook.fr>
2020-12-16 18:02:13 +0100

    rm tuto names

--- tlearning/settings.py
+++ tlearning/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "gettingstarted.urls"
+ROOT_URLCONF = "tlearning.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     }
 ]
 
-WSGI_APPLICATION = "gettingstarted.wsgi.application"
+WSGI_APPLICATION = "tlearning.wsgi.application"
 
 
 # Database
