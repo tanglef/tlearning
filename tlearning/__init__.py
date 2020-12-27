3db1be4175f17ddfa3c053005ea96196d416ab04
Tanguy Lefort <tanguy_l@outlook.fr>
2020-12-27 16:37:36 +0100

    make it work + dropdown menu

--- tlearning/__init__.py
+++ tlearning/__init__.py
@@ -1,12 +1,4 @@
-import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_redis import FlaskRedis
-
-
-# Globally accessible libraries
-db = SQLAlchemy()
-r = FlaskRedis()
 
 
 def init_app():
@@ -14,10 +6,6 @@ def init_app():
 
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_pyfile('config.py')
-
-    # Initialize Plugins
-    db.init_app(app)
-    r.init_app(app)
 
     with app.app_context():  # structure
         # import routes
