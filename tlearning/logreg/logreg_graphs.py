8765ef9721da7d53bb6a27e0ed649f3fc053fb7a
Tanguy Lefort <tanguy_l@outlook.fr>
2021-01-25 17:16:24 +0100

    sidebar with animation + begin logreg contingency

--- tlearning/logreg/logreg_graphs.py
+++ tlearning/logreg/logreg_graphs.py
@@ -1,5 +1,16 @@
 import numpy as np
 import os
+import plotly.graph_objects as go
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 result_dir = os.path.join(current_dir, "static")
+
+np.random.seed(11235)
+
+###############################
+# Sigmoid
+###############################
+
+x = np.array([1, 3.5, 5, 10.75, 15])
+total = np.array([50, 60, 50, 30, 40])
+deads = np.array([3, 9, 7, 8, 13])
