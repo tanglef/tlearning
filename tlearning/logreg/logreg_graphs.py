9a60251cc5d3895ea44d6c216c67cb6b8fd80a7b
Tanguy Lefort <tanguy_l@outlook.fr>
2021-01-28 19:25:00 +0100

    begin multivariate logreg

--- tlearning/logreg/logreg_graphs.py
+++ tlearning/logreg/logreg_graphs.py
@@ -2,8 +2,9 @@ import numpy as np
 import os
 import plotly.graph_objects as go
 import plotly.express as px
-import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 result_dir = os.path.join(current_dir, "static")
@@ -50,16 +51,15 @@ pchat = deads / total
 lr = LinearRegression().fit(all_x.reshape(-1, 1),
                             all_y.reshape(-1, 1))
 y_lin = lr.predict(points_x.reshape(-1, 1)).flatten()
-
 logi = LogisticRegression().fit(all_x.reshape(-1, 1),
-                                all_y.reshape(-1, 1))
+                                all_y.ravel())
 p_logi = sigmoid(points_x * logi.coef_ + logi.intercept_).flatten()
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=x, y=pchat, name=r"MLE(H1) p_c",
                          mode="markers"))
 fig.add_trace(go.Scatter(x=points_x, y=p_logi, line=dict(color='red'),
-                         name=r"logistic model")) # noqa
+                         name=r"logistic model"))  # noqa
 fig.add_trace(go.Scatter(x=points_x, y=y_lin, name="linear model",
                          line=dict(color="blue")))
 fig.update_layout(
@@ -70,4 +70,83 @@ fig.update_layout(
     showlegend=True)
 
 fig.write_html(os.path.join(result_dir, "smokers.html"), auto_open=False,
+               include_plotlyjs="cdn", include_mathjax='cdn')
+
+
+###########################
+# Logreg with 2 variables
+###########################
+
+SEED = 11235
+margin, mesh_size = .6, .3
+X, y = make_classification(n_samples=100,
+                           n_features=2,
+                           n_redundant=0,
+                           n_repeated=0,
+                           n_clusters_per_class=1,
+                           class_sep=.8,
+                           random_state=SEED)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=.3,
+                                                    random_state=SEED)
+
+
+# Create a mesh grid on which we will run our model
+x_min, x_max = X[:, 0].min() - margin, X[:, 0].max() + margin
+y_min, y_max = X[:, 1].min() - margin, X[:, 1].max() + margin
+xrange = np.arange(x_min, x_max, mesh_size)
+yrange = np.arange(y_min, y_max, mesh_size)
+xx, yy = np.meshgrid(xrange, yrange)
+
+# Create classifier, run predictions on grid
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+Z = Z.reshape(xx.shape)
+
+trace_specs = [
+    [X_train, y_train, 0, 'Train', 'square', "#000000", True],
+    [X_train, y_train, 1, 'Train', 'circle', "#f75e5e", True],
+    [X_test, y_test, 0, 'Test', 'square', "#000000", False],
+    [X_test, y_test, 1, 'Test', 'circle', "#f75e5e", False]
+]
+
+fig = go.Figure(data=[
+    go.Scatter(
+        x=X[y == label, 0], y=X[y == label, 1],
+        name=f'Label {label}',
+        mode='markers', marker_symbol=marker,
+        marker_color=col, marker_size=12,
+        showlegend=legend
+    )
+    for X, y, label, _, marker, col, legend in trace_specs
+])
+
+fig.add_trace(
+    go.Contour(
+        x=xrange,
+        y=yrange,
+        z=Z,
+        showscale=True,
+        opacity=0.4,
+        name='Score',
+        hoverinfo='skip',
+        colorbar=dict(nticks=10, ticks='outside',
+                      ticklen=5, tickwidth=1,
+                      showticklabels=True,
+                      tickangle=0, tickfont_size=12,
+                      title="probability to belong in class 1",
+                      titleside='right',
+                      titlefont=dict(size=16,
+                                     family='Arial, sans-serif')
+
+                      )
+    )
+)
+
+fig.update_layout(legend_orientation='h',
+                  title='Classification with logistic regression using two features.')  # noqa
+fig.write_html(os.path.join(result_dir, "binary_classification.html"),
+               auto_open=False,
                include_plotlyjs="cdn", include_mathjax='cdn')
